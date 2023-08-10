ac628bd399d7eb21412a7321abe52a01b71a3fc7
juanile <140908302+juanile@users.noreply.github.com>
2023-08-10 15:08:51 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,10 +1,4 @@
-const btn = document.querySelector("button");
-const div = document.querySelector("div[style='padding: 5px; background-color: green;']");
-
-
-div.addEventListener("click", function() {
-    alert("Hola! Soy el div");
-});
+//solución
 
 btn.addEventListener("click", (event) => {
     event.stopPropagation();
